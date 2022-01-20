7d61628923e9b7f2319ecde814bcda293db583a2
Peter Pecl <88182155+PeroPero1337@users.noreply.github.com>
2022-01-20 22:06:01 +0100

    Roles added
    Added admin and normal user

--- EPlay/Controllers/ProfileController.cs
+++ EPlay/Controllers/ProfileController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EPlay.Models;
+using System.Dynamic;
 
 namespace EPlay.Controllers
 {
@@ -11,11 +12,43 @@ namespace EPlay.Controllers
     {
         public IActionResult Index()
         {
-            using(var db = new EPlayContext())
+            //if (User.IsInRole("Admin"))
+            //    return RedirectToAction("AdminView","Profile");
+            //else
+            //    return RedirectToAction("UserView","Profile");
+
+            return View();
+            
+        }
+
+        //TODO: Change how this works
+        //Create partial for adminview
+
+
+
+        //Admin controlls ------------------------------------------------------------------------------------------------------------------------------
+        public IActionResult AdminView()
+        {
+
+            using (var db = new EPlayContext())
             {
-                var all = db.registered_users.FirstOrDefault(x => x.UserName == User.Identity.Name);
+                var all = db.registered_users.ToList();
                 return View(all);
             }
+
+        }
+
+
+
+        //User controlls ------------------------------------------------------------------------------------------------------------------------------
+        public IActionResult UserView()
+        {
+            using (var db = new EPlayContext())
+            {
+                var single = db.registered_users.FirstOrDefault(x => x.UserName == User.Identity.Name);
+                return View(single);
+            }
+            
         }
     }
 }
