a410c65a0607e6eba0b5cc9ab4619b2b289f976d
Peter Pecl <88182155+PeroPero1337@users.noreply.github.com>
2022-01-26 16:20:37 +0100

    Update 26.1.2022
    Updated design

---
+++ EPlay/Controllers/StaticArticleController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EPlay.Controllers
+{
+    public class StaticArticleController : Controller
+    {
+        public IActionResult LoLS2022DropsRewards()
+        {
+            return View();
+        }
+
+        public IActionResult EslPubgOpenSoon()
+        {
+            return View();
+        }
+
+        public IActionResult EslOneFallQalifiers()
+        {
+            return View();
+        }
+
+        public IActionResult EslOneFallWrapUp()
+        {
+            return View();
+        }
+
+        public IActionResult SimpleNumOne()
+        {
+            return View();
+        }
+    }
+}
